ff50ce835e68c2b9c8bd925930a55b220b65409e
PtrMan <robertw89@googlemail.com>
2018-09-06 21:38:32 +0200

    Add: autoai.Executive

---
+++ src/main/java/org/opennars/lab/autoai/Executive.java
@@ -0,0 +1,38 @@
+package org.opennars.lab.autoai;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Executive {
+    public List<RecordedOperation> recordedOperations = new ArrayList<>();
+
+    // called by op to record a add
+    public void opAdd(String res, String a, String b) {
+        // record add
+        recordedOperations.add(new RecordedOperation(RecordedOperation.EnumType.ADD, a, b, res));
+    }
+
+    // called by op to record a vectorized multiply-add
+    public void opMaddv(String res, String a, String b) {
+        // record madd
+        recordedOperations.add(new RecordedOperation(RecordedOperation.EnumType.MADDV, a, b, res));
+    }
+
+    public static class RecordedOperation {
+        public final EnumType type;
+        public final String a, b, res;
+
+        public RecordedOperation(final EnumType type, final String a, final String b, final String res) {
+            this.type = type;
+            this.a = a;
+            this.b = b;
+            this.res = res;
+        }
+
+        public enum EnumType {
+            ADD, // add
+            MADDV, // vectorized madd
+        }
+
+    }
+}
