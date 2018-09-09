f80930a5842e7aea3168fc19a259d037b0a2125b
PtrMan <robertw89@googlemail.com>
2018-09-09 19:57:01 +0200

    Add: forgot NetworkContext

---
+++ src/main/java/org/opennars/lab/autoai/structure/NetworkContext.java
@@ -0,0 +1,25 @@
+package org.opennars.lab.autoai.structure;
+
+import org.opennars.lab.common.math.DualNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Keeps track of network global information for backproagation and creation
+ */
+public class NetworkContext {
+    public Random rng = new Random();
+
+    /** we need to store which differentiation of any DualNumber is mapped to which value
+     * we are using the index of the DualNumberDiff as a array Index
+     * */
+    public List<DualNumber> mapDiffToDualNumber = new ArrayList<>();
+
+    public int iDiffCounter = 0;
+
+    public int sizeOfDiff = 0;
+
+    public double learnRate = 0;
+}
